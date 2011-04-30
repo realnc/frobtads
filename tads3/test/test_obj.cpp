9244be5b92d3178a062298864b00ca4010fc1278
Nikos Chantziaras <realnc@gmail.com>
2011-04-30 04:10:14 +0300

    Import changed testsuite sources from upstream

--- tads3/test/test_obj.cpp
+++ tads3/test/test_obj.cpp
@@ -24,6 +24,7 @@ Modified
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "t3std.h"
 #include "vmglob.h"
 #include "vmtype.h"
 #include "vmobj.h"
@@ -193,3 +194,7 @@ vm_prop_id_t CVmImageLoader::alloc_new_prop(VMG0_)
     return VM_INVALID_PROP;
 }
 
+vm_prop_id_t CVmImageLoader::get_last_prop(VMG0_)
+{
+    return (vm_prop_id_t)0;
+}
