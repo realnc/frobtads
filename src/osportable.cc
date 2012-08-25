953c7f7cf5bca8ad0b3587a650e2f6e6c9a7e510
Nikos Chantziaras <realnc@gmail.com>
2012-08-25 09:36:41 +0300

    Pass an adjusted group list size to getgroups()
    Since we pass it the address of the second element.  Not strictly needed,
    since getgroups() will not write past the end (we know the contents will
    fit), but it helps getting a cleaner Valgrind run.

--- src/osportable.cc
+++ src/osportable.cc
@@ -578,7 +578,7 @@ os_file_stat( const char *fname, int follow_links, os_file_stat_t *s )
     if (grpSize > NGROUPS_MAX or grpSize < 0)
         return false;
     gid_t* groups = new gid_t[grpSize];
-    if (getgroups(grpSize, groups + 1) < 0) {
+    if (getgroups(grpSize - 1, groups + 1) < 0) {
         delete[] groups;
         return false;
     }
