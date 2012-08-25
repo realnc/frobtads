779ed7e2bf0f7e18b74a8651cb79117b99cb8d34
Nikos Chantziaras <realnc@gmail.com>
2012-08-25 09:10:22 +0300

    Check whether getgroups() succeeded by checking for a negative return value, not 0

--- src/osportable.cc
+++ src/osportable.cc
@@ -578,7 +578,7 @@ os_file_stat( const char *fname, int follow_links, os_file_stat_t *s )
     if (grpSize > NGROUPS_MAX or grpSize < 0)
         return false;
     gid_t* groups = new gid_t[grpSize];
-    if (getgroups(grpSize, groups + 1) != 0)
+    if (getgroups(grpSize, groups + 1) < 0)
         return false;
     groups[0] = getegid();
     int i;
